bb3c4f959985fbd19a8f777ba6e9ee49d241ed4a
Vishal <vishal.avalani@gmail.com>
2016-12-21 21:21:19 +0530

    ASP .NET MVC Day 3

---
+++ FirstDbApp/FirstDbApp.ViewModel/EmployeeVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstDbApp.ViewModel
+{
+    public class EmployeeVM
+    {
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+        public System.DateTime DateOfBirth { get; set; }
+        public int GenderID { get; set; }
+        public System.DateTime DateOfJoining { get; set; }
+        public bool IsActive { get; set; }
+        public int DepartmentID { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+
+
+        public string Gender { get; set; }
+        public string Department { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
---
+++ FirstDbApp/FirstDbApp/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using FirstDbApp.Data;
+using FirstDbApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstDbApp.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EMSTrainingEntities db = new EMSTrainingEntities();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            List<EmployeeVM> EList = (from a in db.Employees
+                                      select new EmployeeVM()
+                                      {
+                                          EmployeeID = a.EmployeeID,
+                                          FirstName = a.FirstName,
+                                          LastName = a.LastName,
+                                          Email = a.Email,
+                                          Address = a.Address,
+                                          ContactNumber = a.ContactNumber,
+                                          DateOfBirth = a.DateOfBirth,
+                                          DateOfJoining = a.DateOfJoining,
+                                          IsActive = a.IsActive,
+                                          DepartmentID = a.DepartmentID,
+                                          GenderID = a.GenderID,
+                                          CreatedDate = a.CreatedDate,
+                                          CreatedBy = a.CreatedBy,
+                                          ModifiedDate = a.ModifiedDate,
+                                          ModifiedBy = a.ModifiedBy,
+                                          Gender = a.Gender.Name,
+                                          Department = a.Department.Name,
+                                      }).ToList();
+            return View(EList);
+        }
+    }
+}
